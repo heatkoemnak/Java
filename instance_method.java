723fd1d8a11e777924a42a576669079170b47887
koemnak <heatkimnak@gmail.com>
2023-02-17 13:46:36 +0700

    instance method

--- instance_method.java
+++ instance_method.java
@@ -1,12 +1,12 @@
 
-public class instance{
+public class instance_method{
 
     int square(int x){
         return x*x;
     }
     public static void main(String[] args){
         int x=5;
-        instance obj = new instance();
+        instance_method obj = new instance_method();
         System.out.println("square of "+x+" is :"+obj.square(5));
     }
 }
