f1767444a95bd934c6815310b7351bea5c7801bf
koemnak <heatkimnak@gmail.com>
2023-03-26 14:34:29 +0700

    commited

---
+++ Array/RandomOperation.java
@@ -0,0 +1,39 @@
+import java.util.Random;
+
+class RandomOperation {
+    // Declare and construct variables
+    Random randomnum = new Random();
+    int fnum, snum, answer;
+    int mathnumber = randomnum.nextInt(20);
+    int newnumber = randomnum.nextInt(20);
+
+    // Declare the operator
+    String[] Operators = { "+", "-", "/", "*" };
+   
+
+    for(
+    int i = 0;i<operators.length;i++)
+    {
+        int randomIndex = randomnum.nextInt(4);
+        
+        if (Operators.equals("+"))
+            answer = fnum + snum;
+        else if (Operators.equals("-"))
+            answer = fnum - snum;
+        else if (Operators.equals("/"))
+            answer = fnum / snum;
+        else if (Operators.equals("*"))
+            answer = fnum * snum;
+
+    }
+
+    // Declare and construct variables of math problem
+    fnum=mathnumber;snum=newnumber;
+
+    // Output of math problem
+    System.out.print(fnum);
+    System.out.print(Operators);
+    System.out.println(snum);
+
+}
+
---
+++ Array/array.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class array{
+    public static void main(String[] args){
+        int currentbalance=100;
+        int currentAsset = 200;
+        int TotalAsset = 300;
+        int bank1;
+        int bank2;
+        int bank3;
+        int bank4;
+        int bank5;
+        
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your decimal number:");
+        int limit = input. nextInt();
+        System.out.println("unsafe");
+
+
+        
+       
+        
+    }
+
+
+}
